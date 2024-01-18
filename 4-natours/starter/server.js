34ef3de7d3b1541e45f40771208e073b381637f2
Bertrand D <b.duclot@gmail.com>
2024-01-18 21:11:02 +0100

    update

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -9,7 +9,13 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD,
 );
 
-mongoose.connect();
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
