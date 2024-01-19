055185930aaf8968f4a83286802c467cb7f02be7
Bertrand D <b.duclot@gmail.com>
2024-01-19 17:33:31 +0100

    update

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -17,41 +17,6 @@ mongoose
   })
   .then(() => console.log('DB connection successful'));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-// Tour model
-const Tour = mongoose.model('Tour', tourSchema);
-
-// Instance of the Tour model
-const testTour = new Tour({
-  name: 'The Forest Hiker',
-  rating: 4.7,
-  price: 497,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('ERROR : ', err);
-  });
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
