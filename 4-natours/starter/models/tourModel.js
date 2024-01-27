70c63a985b402ffbfc20c3f8ed90d09c249dbaa9
Bertrand D <b.duclot@gmail.com>
2024-01-27 12:15:20 +0100

    update

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,9 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxLength: [40, 'A tour name must have less or equal than 40 characters'],
+      minLength: [10, 'A tour name must have more or equal than 40 characters'],
+      //validate: [validator.isAlpha, 'Tour name must only contain characters'],
     },
     slug: String,
     duration: {
@@ -21,43 +25,58 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
-    },
-    ratingsAverage: {
-      type: Number,
-      default: 4.5,
-    },
-    ratingsQuantity: {
-      type: Number,
-      default: 0,
-    },
-    price: {
-      type: Number,
-      required: [true, 'A tour must have a price'],
-    },
-    priceDiscount: Number,
-    summary: {
-      type: String,
-      trim: true,
-      required: [true, 'A tour must have a description'],
-    },
-    description: {
-      type: String,
-      trim: true,
-    },
-    imageCover: {
-      type: String,
-      required: [true, 'A tour must have a cover image'],
-    },
-    images: [String],
-    createAt: {
-      type: Date,
-      default: Date.now(),
-      select: false,
-    },
-    startDates: [Date],
-    secretTour: {
-      type: Boolean,
-      default: false,
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is easy, medium, or difficult',
+      },
+      ratingsAverage: {
+        type: Number,
+        default: 4.5,
+        min: [1, 'Min is 1'],
+        max: [5, 'Max is 5'],
+      },
+      ratingsQuantity: {
+        type: Number,
+        default: 0,
+      },
+      price: {
+        type: Number,
+        required: [true, 'A tour must have a price'],
+      },
+      priceDiscount: {
+        type: Number,
+        validate: {
+          validator: function (val) {
+            // this only points to current doc on NEW document creation
+            return val < this.price;
+          },
+          message: 'Discount price ({VALUE}) should be below regular price',
+        },
+      },
+      summary: {
+        type: String,
+        trim: true,
+        required: [true, 'A tour must have a description'],
+      },
+      description: {
+        type: String,
+        trim: true,
+      },
+      imageCover: {
+        type: String,
+        required: [true, 'A tour must have a cover image'],
+      },
+      images: [String],
+      createAt: {
+        type: Date,
+        default: Date.now(),
+        select: false,
+      },
+      startDates: [Date],
+      secretTour: {
+        type: Boolean,
+        default: false,
+      },
     },
   },
   {
