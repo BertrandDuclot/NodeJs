571f640c972c19488216666c3298f84cc8f66ed5
Bertrand D <b.duclot@gmail.com>
2024-02-14 20:08:36 +0100

    update

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -139,6 +139,8 @@ tourSchema.pre('save', function(next) {
   next();
 });
 
+// Commented Code
+
 // tourSchema.pre('save', async function(next) {
 //   const guidesPromises = this.guides.map(async id => await User.findById(id));
 //   this.guides = await Promise.all(guidesPromises);
